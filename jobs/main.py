60950e199131dd455ae1c1d0d0ee4b3676e76af2
Chukwuemeka <mekastans@gmail.com>
2024-02-19 16:11:44 +0100

    updated main.py

--- jobs/main.py
+++ jobs/main.py
@@ -9,13 +9,13 @@ import uuid
 from confluent_kafka import SerializingProducer
 import simplejson as json
 
-# Setting the Lattitude & Longitude for both locations
-CITY_A_COORDINATES = {"lattitude": 51.5074, "longitude": -0.1278}
-CITY_B_COORDINATES = {"lattitude": 52.4862, "longitude": -1.8904}
+# Setting the latitude & Longitude for both locations
+CITY_A_COORDINATES = {"latitude": 51.5074, "longitude": -0.1278}
+CITY_B_COORDINATES = {"latitude": 52.4862, "longitude": -1.8904}
 
 # Calculate movement per increments
-LATITUDE_INCREMENT = (CITY_B_COORDINATES['lattitude'] 
-                      - CITY_A_COORDINATES["lattitude"]) / 100
+LATITUDE_INCREMENT = (CITY_B_COORDINATES['latitude'] 
+                      - CITY_A_COORDINATES["latitude"]) / 100
 LONGITUDE_INCREMENT = (CITY_B_COORDINATES['longitude'] 
                       - CITY_A_COORDINATES["longitude"]) / 100
 
@@ -27,16 +27,18 @@ TRAFFIC_TOPIC = os.getenv('TRAFFIC_TOPIC', 'traffic_data')
 WEATHER_TOPIC = os.getenv('WEATHER_TOPIC', 'weather_data')
 EMERGENCY_TOPIC = os.getenv('EMERGENCY_TOPIC', 'emergency_data')
 
+random.seed(42)
 # Setting start details [time & location]
 start_time = datetime.now()
 start_location = CITY_A_COORDINATES.copy()
 
+# ---------------- FOR GENERATING VEHICLE DATA -----------------------
 
 def get_next_time():
     """_summary_
-    This function is used to generate new timestamp for each movement of the vehicle.
-    It increments the current start_time by a random number from 30 - 60 seconds & 
-    add to it. 
+        This function is used to generate new timestamp for each movement of the vehicle.
+        It increments the current start_time by a random number from 30 - 60 seconds & 
+        add to it. 
 
     Returns:
         It returns the new start_time
@@ -50,10 +52,10 @@ def get_next_time():
 # Simulating a moving vehicle
 def simulate_vehicle_movement():
     """_summary_
-    This function simulates a moving vehicle by increasing the start_loaction's
-    longitude & latitude respectively. Although in a real world setting this data
-    is meant to be gotten from e.g. IOT devices. This is hardcoded in to enable simulation
-    and randomness is also included with a range to avoid anomaly in data generation.
+        This function simulates a moving vehicle by increasing the start_loaction's
+        longitude & latitude respectively. Although in a real world setting this data
+        is meant to be gotten from e.g. IOT devices. This is hardcoded in to enable simulation
+        and randomness is also included with a range to avoid anomaly in data generation.
 
     Returns:
         It returns the new start_location details
@@ -61,11 +63,11 @@ def simulate_vehicle_movement():
     global start_location
     
     # Move towards destination
-    start_location['lattitude'] += LATITUDE_INCREMENT
+    start_location['latitude'] += LATITUDE_INCREMENT
     start_location['longitude'] += LONGITUDE_INCREMENT
     
     # Adding randomness to simulate actual road travel
-    start_location['lattitude'] += random.uniform(-0.0005, 0.0005)
+    start_location['latitude'] += random.uniform(-0.0005, 0.0005)
     start_location['longitude'] += random.uniform(-0.0005, 0.0005)
     
     return start_location
@@ -73,15 +75,65 @@ def simulate_vehicle_movement():
 
 # Generating vehicle data
 def generate_vehicle_data(device_id):
+    """_summary_
+        This function gets the newly generated location details & timestamp.
+        It uses this new details to generate the car data. 
+
+    Returns:
+        It returns the vehicle's data.
+    """
     location = simulate_vehicle_movement()
     
     return {
         'id': uuid.uuid4(),
         'deviceId': device_id,
-        'timestamp': get_next_time().isoformat()
+        'timestamp': get_next_time().isoformat(),
+        'location': (location['latitude'], location['longitude']),
+        'speed': random.uniform(10, 40),
+        'direction': 'North-East',
+        'make': 'BMW',
+        'model': 'C500',
+        'year': 2024,
+        'fuelType': 'Hybrid'
     }
 
 
+# ---------------- FOR GENERATING GPS DATA -----------------------
+
+def generate_gps_data(device_id, timestamp, vehicle_type='private'):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'timestamp': timestamp,
+        'speed': random.uniform(0, 40),
+        'direction': 'North-East',
+        'vehicleType': vehicle_type
+    }
+    
+# ---------------- FOR GENERATING TRAFFIC CAMERA DATA -----------------------
+
+def generate_traffic_camera_data(device_id, timestamp, location, camera_id):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'cameraId': camera_id,
+        'timestamp': timestamp,
+        'location': location,
+        'snapshot': 'Base64EncodedString'
+    }
+
+
+# ---------------- FOR GENERATING WEATHER DATA -----------------------
+def generate_weather_data(device_id, timestamp, location):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'location': location,
+        'timestamp': timestamp,
+        'temperature' : random.uniform(-5, 26),
+        'weatherCondition' : random.choice(['Sunny', 'Cloudy', 'Rain', 'Snow'])
+    }
+    
 
 # Simulating a journey
 def simulate_journey(producer, device_id):
@@ -89,6 +141,10 @@ def simulate_journey(producer, device_id):
     
     while driving:
         vehicle_data = generate_vehicle_data(device_id)
+        gps_data = generate_gps_data(device_id, vehicle_data['timestamp'])
+        traffic_camera_data = generate_traffic_camera_data(device_id, vehicle_data['timestamp'], vehicle_data['location'], 'Camer123')
+        weather_data =  generate_weather_data(device_id, vehicle_data['timestamp'], vehicle_data['location'])
+        
     
     
 
